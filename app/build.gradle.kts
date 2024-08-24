81d39e37dd2befca5c35d41a871475bf6762cd66
Abhishek Suman <abhisheksuman413@gmail.com>
2024-08-24 21:09:42 +0530

    Splash Screen

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.fps69.abworkmanager"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.fps69.abworkmanager"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding =true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // dependencies For Firebase
+    implementation("com.google.firebase:firebase-bom:33.1.2")
+    implementation ("com.google.firebase:firebase-auth-ktx:21.0.7")
+    // Firebase Storage
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    // Firebase Realtime Database
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
+
+    // This is dependencies for kotlin coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //Glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit Gson Converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Circle Image
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
+    // Text Dimension
+    implementation ("com.intuit.ssp:ssp-android:1.1.1")
+    implementation ("com.intuit.sdp:sdp-android:1.1.1")
+
+
+
+}
---
+++ app/src/main/java/com/fps69/abworkmanager/auth/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.fps69.abworkmanager.auth
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.fps69.abworkmanager.MainActivity
+import com.fps69.abworkmanager.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+            finish()
+        },5000)
+
+    }
+}
