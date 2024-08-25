f271ed1adff812923812849749416bb8832d0a1d
Abhishek Suman <abhisheksuman413@gmail.com>
2024-08-25 20:39:02 +0530

    Design SignUp Page

--- app/src/main/java/com/fps69/abworkmanager/auth/SplashScreenActivity.kt
+++ app/src/main/java/com/fps69/abworkmanager/auth/SplashScreenActivity.kt
@@ -18,7 +18,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+            startActivity(Intent(this@SplashScreenActivity,SignUpActivity::class.java))
             finish()
         },5000)
 
